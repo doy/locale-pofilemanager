ad2ed3e6b53d86e46a789a8ac1833c0349d3c1ba
Jesse Luehrs <doy@tozt.net>
2010-02-17 10:42:08 -0600

    initial commit

---
+++ lib/Locale/POFileManager.pm
@@ -0,0 +1,71 @@
+package Locale::POFileManager;
+use Moose;
+
+=head1 NAME
+
+Locale::POFileManager -
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+=head1 BUGS
+
+No known bugs.
+
+Please report any bugs through RT: email
+C<bug-locale-pofilemanager at rt.cpan.org>, or browse to
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Locale-POFileManager>.
+
+=head1 SEE ALSO
+
+
+=head1 SUPPORT
+
+You can find this documentation for this module with the perldoc command.
+
+    perldoc Locale::POFileManager
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Locale-POFileManager>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Locale-POFileManager>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Locale-POFileManager>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Locale-POFileManager>
+
+=back
+
+=head1 AUTHOR
+
+  Jesse Luehrs <doy at tozt dot net>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2009 by Jesse Luehrs.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as perl itself.
+
+=cut
+
+1;
